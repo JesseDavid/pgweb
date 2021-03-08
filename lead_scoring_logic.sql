165bcabb01424c187e67a50a322ce200100f5e3b
Jesse David <jesse.p.david@gmail.com>
2021-03-08 09:26:21 -0600

    add in better log messages

---
+++ lead_scoring_logic.sql
@@ -0,0 +1,44 @@
+CREATE OR REPLACE FUNCTION set_lead_score(
+    current_rating NUMERIC,
+    has_budget BOOLEAN,
+    rating TEXT,
+    city TEXT) 
+RETURNS NUMERIC AS $$
+DECLARE newScore NUMERIC;
+BEGIN
+    newScore := current_rating;
+    
+    IF has_budget THEN
+      newScore := newScore + 5;
+    ELSE
+      newScore := newScore - 10;
+    END IF;
+    
+    IF rating = 'Hot' THEN
+      newScore := newScore + 7;
+    ELSIF rating = 'Warm' THEN
+      newScore := newScore;
+    ELSE 
+      newScore := newScore - 8;
+    END IF;
+    
+    IF city IS NULL THEN
+      newScore := newScore - 2;
+    END IF;  
+    
+    
+    RETURN newScore;
+END; $$
+
+LANGUAGE plpgsql;
+
+create or replace procedure update_leads()
+language plpgsql    
+as $$
+begin
+   UPDATE salesforce.lead
+    SET lead_score_value__c = set_lead_score( lead_score_value__c, has_budget__c, rating, city )
+    WHERE  lastname LIKE 'T%';
+
+    commit;
+end;$$
--- run_lead_scoring.js
+++ run_lead_scoring.js
@@ -7,13 +7,16 @@ const client = new Client({
       }
 });
 
+console.log('connecting to db...');
 client.connect();
+console.log('...connected');
 
+console.log('Running Lead score processing...');
 client.query('call update_leads();', (err, res) => {
     if (err) throw err;
     for (let row of res.rows) {
       console.log(JSON.stringify(row));
     }
-    console.log('cutting connection');
+    console.log('...done lead processing');
     client.end();
 });
